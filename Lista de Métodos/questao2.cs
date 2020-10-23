5c84da0f1cc8843c0e6a8208db31bb0241426bfe
JacksonRicardo <42697964+JacksonRicardo@users.noreply.github.com>
2020-10-23 17:50:18 -0300

    Questão 2

---
+++ Lista de Métodos/questao2.cs
@@ -0,0 +1,46 @@
+using System;
+	
+
+	    class Program
+	    {
+	        static void Main(string[] args)
+	        {
+	            Console.WriteLine("Digite os valores: ");
+	            int x = int.Parse(Console.ReadLine());
+	            int y = int.Parse(Console.ReadLine());
+	            int z = int.Parse(Console.ReadLine());
+	           
+	            Ordenar(true, ref x, ref y, ref z);
+	            Console.WriteLine($"Ordem Crescente  : {x} {y} {z}");
+	            
+	            Ordenar(false, ref x, ref y, ref z);
+	            Console.WriteLine($"Ordem Decrescente: {x} {y} {z}");
+	            Console.ReadKey();
+	        }
+	        static void Ordenar(bool crescente, ref int x, ref int y, ref int z)
+	        {
+	            
+	            int a;
+	            if (crescente)
+	            {
+	                if (x > y) { a = x; x = y; y = a; }
+	
+
+	                if (x > z) { a = x; x = z; z = a; }
+	
+
+	                if (y > z) { a = y; y = z; z = a; }
+	            }
+	            else
+	            {
+	                if (x < y) { a = x; x = y; y = a; }
+	
+
+	                if (x < z) { a = x; x = z; z = a; }
+	
+
+	                if (y < z) { a = y; y = z; z = a; }
+	            }
+	        }
+	    }
+
